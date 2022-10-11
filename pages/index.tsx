090aecde2b52554a2fb2c287002833db7d1d2639
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 13:53:24 -0700

    Created Skills.tsx outline of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import About from '../components/About';
-import Header from '../components/Header';
+import type { NextPage } from "next";
+import Head from "next/head";
+import About from "../components/About";
+import Header from "../components/Header";
 import Hero from "../components/Hero";
-import WorkExperience from '../components/WorkExperience';
+import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
+
 
 const Home: NextPage = () => {
   return (
@@ -22,12 +24,13 @@ const Home: NextPage = () => {
         <About />
       </section>
 
-      {/* Experience */}
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
 
-      {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
 
