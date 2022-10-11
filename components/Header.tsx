f40c1eb6e40e1e79a314e43b3f2d968ff58d561a
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-10 18:32:48 -0700

    installed framer-motion /header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,11 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <div className='flex flex-row items-center'>
             {/* Social Icons */}
             <SocialIcon 
                 url="https://github.com/marioebros" 
-                fgColor='silver'
-                bgColor='transparent'
-            />
-            <SocialIcon 
-                url="https://twitter.com/MmeDollarize" 
                 fgColor='silver'
                 bgColor='transparent'
             />
@@ -24,10 +19,26 @@ export default function Header({}: Props) {
                 bgColor='transparent'
             />
             <SocialIcon 
-                url="https://twitter.com/jaketrent" 
+                url="https://twitter.com/MmeDollarize" 
+                fgColor='silver'
+                bgColor='transparent'
+                
+            />
+            <SocialIcon 
+                url="https://www.instagram.com/issa_mmeh/" 
                 fgColor='silver'
                 bgColor='transparent'
             />
+        </div>
+
+        <div>
+            <SocialIcon
+                className='cursor-pointer'
+                network='email'
+                fgColor='silver'
+                bgColor='transparent' 
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-silver-400'>Get In Touch!</p>
         </div>
     </header>
   );
