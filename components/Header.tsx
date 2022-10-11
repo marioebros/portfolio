10676f1c85d15f4a0e815ca9ad08af951f27ec75
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-10 18:53:09 -0700

    finished header with tailwind + framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <motion.div className='flex flex-row items-center'>
-            
+        <motion.div 
+        initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5
+        }}
+        animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+        }}
+        transition={{
+            duration: 1.5,
+        }}
+        className='flex flex-row items-center'>
             
             {/* Social Icons */}
             <SocialIcon 
@@ -33,15 +47,32 @@ export default function Header({}: Props) {
             />
         </motion.div>
 
-        <div>
+        <motion.div
+            initial={{
+                x: 500,
+                opacity: 0,
+                scale: 0.5,
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1,
+            }}
+            transition={{
+                duration: 1.5,
+            }}
+            className="flex flex-row items-center text-silver-300 cursor-pointer"
+            >
             <SocialIcon
                 className='cursor-pointer'
                 network='email'
                 fgColor='silver'
                 bgColor='transparent' 
             />
-            <p className='uppercase hidden md:inline-flex text-sm text-silver-400'>Get In Touch!</p>
-        </div>
+            <p className='uppercase hidden md:inline-flex text-sm text-silver-400'>
+                Get In Touch!
+            </p>
+        </motion.div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { motion } from "framer-motion"
+// import { motion } from "framer-motion"
 import Head from 'next/head';
 import Header from '../components/Header';
 
