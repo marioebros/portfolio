2b76da52e1d597db6dc4c2afb2ea6635b0be897d
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 15:06:07 -0700

    Updated skills + skill div. Next-env off gitignore

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Skill from "./Skill";
 
 type Props = {}
 
 function Skills({}: Props) {
   return (
-    <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <motion.div 
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl">
             Skills
         </h3>
@@ -15,7 +20,22 @@ function Skills({}: Props) {
         </h3>
 
         <div className="grid grid-cols-4 gap-5">
-            <Skills />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
         </div>
     </motion.div>
   );
