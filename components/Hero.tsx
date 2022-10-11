5b855f8194d7af88174b76d3fcfb013fbbe36084
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-10 20:22:11 -0700

    created BackgroundCircles animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from "./BackgroundCircles";
 
 type Props = {}
 
@@ -16,11 +17,12 @@ export default function Hero({}: Props) {
     });
 
   return (
-    <div>
-        <h1>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+      <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
-        </h1>
+      </h1>
     </div>
     );
 }
