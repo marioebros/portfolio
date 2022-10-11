14ecf154192c3ec0e0f85240945a1ff65a3040e3
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 13:04:33 -0700

    changed bg color of cards and finished styling

--- components/About.tsx
+++ components/About.tsx
@@ -25,7 +25,7 @@ export default function About({}: Props) {
         }}
         whileInView={{ opacity: 1, x:0 }}
         viewport={{ once: true }}
-        src='/components/assets/portfolio-about-profile.png' 
+        src="/assets/portfolio-about-profile.png"
         className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
         />
 
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#12221D] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
         <motion.img 
             initial={{
                 y: -100,
@@ -19,16 +19,43 @@ export default function ExperienceCard({}: Props) {
             alt=""
         />
 
-        <div>
-            <h4>Self-Employed Web Developer</h4>
-            <p>Mario M. Elizalde</p>
-            <div>
+        <div className="px-0 md:px-10">
+            <h4 className="text-4xl font-light">Self-Employed Web Developer</h4>
+            <p className="font-bold text-2xl mt-1">Mario M. Elizalde</p>
+            <div className="flex space-x-2 my-2">
+                <img
+                    className="h-10 w-10 rounded-full"
+                    src=""
+                    alt=""
+                />
+                <img
+                    className="h-10 w-10 rounded-full"
+                    src=""
+                    alt=""
+                />
+                <img
+                    className="h-10 w-10 rounded-full"
+                    src=""
+                    alt=""
+                />
+                <img
+                    className="h-10 w-10 rounded-full"
+                    src=""
+                    alt=""
+                />
+                <img
+                    className="h-10 w-10 rounded-full"
+                    src=""
+                    alt=""
+                />
                 {/* Tech Used */}
                 {/* Tech Used */}
                 {/* Tech Used */}
                 {/* Tech Used */}
             </div>
-            <p>Started Work... -Ended...</p>
+            <p className="uppercase py-5 text-gray-300">
+                Started Work... Ended...
+            </p>
 
             <ul className="list-disc space-y-4 ml-5 text-lg">
                 <li>Summary Points</li>
