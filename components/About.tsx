8bef1769332cfce81e5af8ab3efee45813a72eec
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 06:27:18 -0700

    created motion.img element and upload about pic

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
 
 type Props = {}
 
 export default function About({}: Props) {
-  return <div className='flex flex-col relative h-screen text-screen md:text-left md:flex-row'>About</div>;
+  return (
+    <div className='flex flex-col relative h-screen text-screen md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl'>
+            About
+        </h3>
+
+        <motion.img 
+        initial={{
+            x: -200,
+        }}
+        whileInView={{ x:0 }}
+        viewport={{ once: true }}
+        src='' 
+        />
+    </div>
+    );
 }
