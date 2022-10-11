4940632f1445ad5480bc2d4229ebbe7fdfab77e2
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-10 22:06:23 -0700

    added font styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,10 +19,17 @@ export default function Hero({}: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles />
-        <img src="./components/assets/slack-profile.png" alt='' />
+        <img 
+            className="relative rounded-full h-32 w-32 mx-auto object-cover"
+            src="../components/assets/slack-profile.png" 
+            alt='' 
+        />
         <div>
-            <h1>
-                <span>{text}</span>
+            <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+                Software Developer
+            </h2>
+            <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+                <span className='mr-3'>{text}</span>
                 <Cursor cursorColor="#F7AB0A" />
             </h1>
         </div>
