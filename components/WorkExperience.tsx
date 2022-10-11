db77dc4e3320baa7f3582e023a53294cde882003
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 09:07:43 -0700

    created WorkExperience.tsx and updated snap-center

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <div>WorkExperience</div>
-  )
+    <div className='h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl'>
+            Experience
+        </h3>
+    </div>
+    );
 }
 
 export default WorkExperience
