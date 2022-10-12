c3121be3fe16dd782f8413ec5cb4211f669de16e
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 20:13:19 -0700

    Finished Projects.tsx styling and animations

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,16 @@
-import React from "react"
+import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {}
 
 function Projects({}: Props) {
     const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <motion.div 
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl">
             Projects
         </h3>
@@ -13,8 +18,16 @@ function Projects({}: Props) {
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
             {projects.map((project, i) => (
                 <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                    <img src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf912a4a0795b5022b6be-666x375.pgn" 
-                         alt="" />
+                    <motion.img 
+                        initial={{
+                            y: -300,
+                            opacity: 0,
+                        }}
+                        transition={{ duration: 1.2 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{  once: true }}
+                        src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf912a4a0795b5022b6be-666x375.pgn" 
+                        alt="" />
 
                     <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                         <h4 className="text-4xl font-semibold text-center">
@@ -23,13 +36,17 @@ function Projects({}: Props) {
                             </span>{" "} 
                             G's Restaurant Site
                         </h4>
+
+                        <p className="text-lg text-center md:text-left">
+                            Words yo.
+                        </p>
                     </div>
                 </div>
             ))}
         </div>
 
         <div className="w-full absolute top-[25%] bg-[#FFCC00]/50 left-0 h-[500px] -skew-y-12 -skew-x-12" />
-    </div>
+    </motion.div>
   );
 }
 
