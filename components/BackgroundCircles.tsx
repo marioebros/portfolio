8961407cb4792d07809939f02d5d15c8787366f1
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-11 19:00:32 -0700

    New color scheme in bg circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -19,12 +19,12 @@ function BackgroundCircles({}: Props) {
         }}
         className='relative flex justify-center items-center'
     >
-        <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
-        <div className='rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52' />
-        <div className='rounded-full border border-[#333333] h-[500px] w-[500px] absolute mt-52' />
-        <div className='rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse' />
-        <div className='rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52'/>
-    </motion.div>
+        <div className='absolute border border-[#C8102E] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
+        <div className='rounded-full border border-[#E5E4E2] h-[300px] w-[300px] absolute mt-52' />
+        <div className='rounded-full border border-[#006341] h-[500px] w-[500px] absolute mt-52' />
+        <div className='rounded-full border border-[#E5E4E2] opacity-100 h-[650px] w-[650px] absolute mt-52 animate-pulse' />
+        <div className='rounded-full border border-[#FFCC00] h-[800px] w-[800px] absolute mt-52'/>
+    </motion.div> // #C8102E - Red, #E5E4E2 - Platinum, #006341 - Dark Green, #FFCC00 - USC Gold
   );
 }
 
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,9 +13,10 @@ function Projects({}: Props) {
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
             {projects.map((project, i) => (
                 <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                    <img src="" alt="" />
+                    <img src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf912a4a0795b5022b6be-666x375.pgn" 
+                         alt="" />
 
-                    <div>
+                    <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                         <h4 className="text-4xl font-semibold text-center">
                             <span className="underline decoration-[#F7AB0A]/50">
                                 Case Study {i+1} of {projects.length}:
