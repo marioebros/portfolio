6e0e4b8c91cf94e86b011cb85eec3c3dc7d2a27a
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-12 20:40:54 -0700

    Created schemas for portfolio sections

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,7 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // We import object and document schemas
-import blockContent from "./blockContent";
-import category from "./category";
 import pageInfo from "./pageInfo";
-import author from "./author";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,10 +17,8 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     pageInfo,
-    author,
-    category,
     // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
+    // { type: "typename" } in other document schemas
     blockContent,
   ]),
 });
