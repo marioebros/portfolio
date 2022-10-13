ba5d5c3757070ea4dd97e27351e3e9b6be3d4fa9
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-13 12:41:22 -0700

    Fixed social.js

--- sanity/schemas/social.js
+++ sanity/schemas/social.js
@@ -1,52 +1,18 @@
 export default {
-  name: "experience",
-  title: "Experience",
+  name: "social",
+  title: "Social",
   type: "document",
   fields: [
     {
-      name: "jobTitle",
-      title: "JobTitle",
+      name: "title",
+      title: "Title",
+      description: "platform for social media",
       type: "string",
     },
     {
-      name: "companyImage",
-      title: "Company Image",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: "company",
-      title: "Company",
-      type: "text",
-    },
-    {
-      name: "dateStarted",
-      title: "DateStarted",
-      type: "date",
-    },
-    {
-      name: "dateEnded",
-      title: "DateEnded",
-      type: "date",
-    },
-    {
-      name: "isCurrentlyWorkingHere",
-      title: "IsCurrentlyWorkingHere",
-      type: "boolean",
-    },
-    {
-      name: "technologies",
-      title: "Technologies",
-      type: "array",
-      of: [{ type: "reference", to: { type: "skill" } }],
-    },
-    {
-      name: "points",
-      title: "Points",
-      type: "array",
-      of: [{ type: "string" }],
+      name: "url",
+      title: "Url",
+      type: "url",
     },
   ],
 };
