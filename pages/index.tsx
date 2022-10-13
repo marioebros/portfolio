946b2bd857b8888a1acc644fc338bead5435828e
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-12 18:26:29 -0700

    Installed sanity.io to portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -40,8 +41,19 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0" 
+              src="" 
+              alt="" />
+          </div>
+        </footer>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home
