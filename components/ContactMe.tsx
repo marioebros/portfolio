95bdf3d55a0161254ae1ba2b009a219544778990
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-20 20:20:06 -0700

    Updated ContactMe text form input styling

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -29,7 +29,7 @@ function ContactMe({}: Props) {
 
         <div className="flex flex-col space-y-10">
             <h4 className="text-4xl font-semibold text-center">
-                I've got just what you need.{" "}
+                {" "}
                 <span className="decoration-[#FFCC00]/50 underline">Let's Talk.</span>
             </h4>
 
