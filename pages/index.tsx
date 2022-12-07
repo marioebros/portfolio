ffe3e49cfef5baa828a2ae98969eabda753ead43
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-06 17:17:28 -0800

    Added <picture> in conjunction w/<img> line 68+74

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,11 +65,13 @@ const Home = ({pageInfo, experiences, projects, skills, socials}: Props) => {
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
             // eslint-disable-next-line react/jsx-no-undef
-            <Image
+            <picture>
+              <img
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0" 
               src="../assets/2.png"
               alt="" 
             />
+            </picture>
           </div>
         </footer>
       </Link>
