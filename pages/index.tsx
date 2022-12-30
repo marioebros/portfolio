436c0f1489af8216cbcc780eb7c0c9c608eee890
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-29 17:52:59 -0800

    Del // eslint text from below link=#hero Contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,6 @@ const Home = ({pageInfo, experiences, projects, skills, socials}: Props) => {
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            // eslint-disable-next-line react/jsx-no-undef
             <picture>
               <img
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0" 
